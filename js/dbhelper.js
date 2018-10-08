b31014afc225062b07b3b88ed26b89d153ca6c6e
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-10-08 01:12:29 -0500

    Improve accessibility

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -154,6 +154,13 @@ class DBHelper {
   }
 
   /**
+   * Restaurant image alt URL.
+   */
+  static imageAltUrlForRestaurant(restaurant) {
+    return (`${restaurant.alt}`);
+  }
+  
+  /**
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
